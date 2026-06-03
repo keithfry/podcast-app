9d87f4b24f0e5870f238585807843197be0c3a01
keithfry <keithfry@gmail.com>
2026-06-03 19:33:39 -0400

    feat: Hilt module binding TextSummarizer to GemmaTextSummarizer

---
+++ app/src/main/kotlin/com/frybynite/podcastapp/deepdive/DeepDiveModule.kt
@@ -0,0 +1,14 @@
+package com.frybynite.podcastapp.deepdive
+
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class DeepDiveModule {
+    @Binds @Singleton
+    abstract fun bindTextSummarizer(impl: GemmaTextSummarizer): TextSummarizer
+}
